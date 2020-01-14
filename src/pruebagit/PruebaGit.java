552f9e35f19e7b1eaa73b8c0c63981efb26db7ca
fptar <fptar@LAPTOP-E30A06OV>
2020-01-14 20:09:35 +0100

    Prueba

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -10,12 +10,25 @@ package pruebagit;
  * @author fptar
  */
 public class PruebaGit {
+    
+      public boolean multiploMultiple(int numero){
+        if (numero % 3 == 0 && numero % 5 == 0){ //el número es múltiplo de 3 Y de 5
+            return false;
+        }
+        if (numero % 3 == 0 || numero % 5 == 0){ //el número es múltiplo de 3 ó
+                                                 //de 5 pero no de los dos, porque si fuera de los dos se habría salido ya en el anterior if
+            return true;
+        }
+        return false;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("prueba git");
+       
+        // te camelo!!
+       
     }
     
 }
